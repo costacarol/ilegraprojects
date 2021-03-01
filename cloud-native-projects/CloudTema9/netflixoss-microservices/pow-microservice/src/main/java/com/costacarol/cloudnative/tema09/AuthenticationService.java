7de27403601b6151a643f112216b3f9aae30752d
Carolina Costa <69811301+costacarol@users.noreply.github.com>
2021-03-01 15:52:50 -0300

    Delete cloud-native-projects/CloudTema9 directory

--- cloud-native-projects/CloudTema9/netflixoss-microservices/pow-microservice/src/main/java/com/costacarol/cloudnative/tema09/AuthenticationService.java
+++
@@ -1,9 +0,0 @@
-package com.costacarol.cloudnative.tema09;
-
-import io.netty.buffer.ByteBuf;
-import io.reactivex.netty.protocol.http.server.HttpServerRequest;
-import rx.Observable;
-
-public interface AuthenticationService {
-    Observable<Boolean> authenticate(HttpServerRequest<ByteBuf> request);
-}
